73c5db34757f831e41eb2ff40b315d7f62f40d78
Willy Tarreau <w@1wt.eu>
2020-08-14 14:52:24 +0200

    WIP: random32: remove net_rand_state leftovers

--- include/linux/prandom.h
+++ include/linux/prandom.h
@@ -20,8 +20,6 @@ struct rnd_state {
 	__u32 s1, s2, s3, s4;
 };
 
-DECLARE_PER_CPU(struct rnd_state, net_rand_state);
-
 u32 prandom_u32_state(struct rnd_state *state);
 void prandom_bytes_state(struct rnd_state *state, void *buf, size_t nbytes);
 void prandom_seed_full_state(struct rnd_state __percpu *pcpu_state);
